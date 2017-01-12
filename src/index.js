eb9396a2ccc74fae1b07a101e777609059eee0e9
Michael <mishamihail@gmail.com>
2017-01-13 00:26:36 +0300

    Fix Heroku deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
+import Weather from './components/Weather'
 import './styles/app.scss'
+
 
 render(<Weather/>, document.getElementById('root'))
 
